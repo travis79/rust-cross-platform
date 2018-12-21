9cdccff5f08cb66113c8dd2c9206f368e17d3248
Travis Long <trav.long79@gmail.com>
2018-12-21 13:42:45 -0600

    Add android studio project

---
+++ android/Greetings/app/src/main/java/com/mozilla/greetings/GreetingsActivity.kt
@@ -0,0 +1,12 @@
+package com.mozilla.greetings
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GreetingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_greetings)
+    }
+}
